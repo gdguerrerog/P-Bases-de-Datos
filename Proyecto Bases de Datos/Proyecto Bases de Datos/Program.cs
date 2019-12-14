412b7c887c279c6466bbd109ed912efad98e5850
gdguerrerog <gdguerrerog@unal.edu.co>
2019-12-14 16:02:21 -0500

    :tada::pencil:

---
+++ Proyecto Bases de Datos/Proyecto Bases de Datos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Proyecto_Bases_de_Datos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
