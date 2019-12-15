82ab1ee30b13dfb2e8aa2866a0915a7857d66f55
gdguerrerog <gdguerrerog@unal.edu.co>
2019-12-14 19:16:59 -0500

    :art: Project Structure again

--- Proyecto Bases de Datos/Proyecto Bases de Datos/Logic/Program.cs
+++ Proyecto Bases de Datos/Proyecto Bases de Datos/Logic/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using Proyecto_Bases_de_Datos.DataAccess;
 
-namespace Proyecto_Bases_de_Datos.logic
+namespace Proyecto_Bases_de_Datos.Logic
 {
     class Program
     {
         static void Main(string[] args)
         {
+            new DatabaseLogic(new ConnectionMySQL()).Begin();
+
             Console.WriteLine("Hello World!");
             Console.ReadKey();
+
+
         }
     }
 }
